5c78d7cc1e882422510986ad482de336c15cf0d2
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-09-06 03:37:07 +0530

    Navbar-myishtyle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React,{useState,useRef,useEffect} from "react";
 import classes from "./Navbar.module.css";
 import NavBrand from "./NavBrand";
 import NavItem from "./NavItem";
+import NavActive from "./NavActive";
 
 const Navbar = () => {
+
+  const [pos, setpos] = useState(0);
+  const [Click, setClick] = useState(0);
+  const Buttonref = useRef(null);
+
+  const setactive = (e,index) => {
+    setClick(index);
+  }
+
+  useEffect(() => {
+    console.log(Buttonref.current.getBoundingClientRect());
+    let right = Buttonref.current.getBoundingClientRect().x;
+    setpos({
+      right: right,
+      width: Buttonref.current.getBoundingClientRect().width
+    });
+  });
+
+  const NavInfo = [
+    {
+      "to" : "/",
+      "name" : "Home",
+    },
+    {
+      "to" : "/teams",
+      "name" : "Teams",
+    },
+    {
+      "to" : "/leads",
+      "name" : "Leads",
+    },
+    {
+      "to" : "/events",
+      "name" : "Events",
+    }
+  ]
+
   return (
     <nav className={classes["nav-container"]}>
       <NavBrand logo={"/assets/ieeecs_logo.svg"} />
       <div className={classes["nav-links"]}>
-        <NavItem to="/">Home</NavItem>
-        <NavItem to="/teams">Our Team</NavItem>
-        <NavItem to="/leads">Leads</NavItem>
-        <NavItem to="/events">Events</NavItem>
+        <NavActive right={pos.right} width={pos.width} styles={{right: {pos}}}/>
+        {NavInfo.map((item, index) => {
+          return(
+            <NavItem key={index} innerRef ={index == Click? Buttonref : null} to={item.to} onClick={(e) => setactive(e,index)}>
+              {item.name}
+            </NavItem>
+          )
+        })}
       </div>
     </nav>
   );
