9ce2c05d9b2023e1ab0907e719e8f0328a5e3fa0
Husain Shahid Rao <husain@pop-os.localdomain>
2021-09-07 22:29:53 +0530

    team section basic css

--- src/components/TeamsSection/TabContent.jsx
+++ src/components/TeamsSection/TabContent.jsx
@@ -3,16 +3,18 @@ import classes from "./TeamsSection.module.css";
 
 const TabContent = (props) => {
   return (
-    <div>
-      <p>{props.html}</p>
+    <div className={classes["content-container"]}>
+      <p className={classes.text}>{props.html}</p>
+      <div className={classes["members-container"]}>
       {props.members.map((member) => {
         return (
-          <div className={`${member.head && classes['team-head']}`}>
-            <img src={member.img} alt={member.name} />
-            <p>{member.name}</p>
+          <div className={`${classes.members}  ${member.head && classes["team-head"]}`}>
+            <img className={classes["member-image"]} src={member.img} alt={member.name} />
+            <p className={classes["member-name"]}>{member.name}</p>
           </div>
         );
       })}
+      </div>
     </div>
   );
 };
