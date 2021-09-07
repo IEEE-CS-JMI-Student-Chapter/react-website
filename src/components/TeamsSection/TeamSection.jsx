d0bee47dc0ce95b94b63b673a6d0ee67991138b9
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-09-07 11:53:02 +0530

    Added TeamsSection and LandingSection files

---
+++ src/components/TeamsSection/TeamSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+import classes from "./TeamsSection.module.css";
+
+// const Tea
+
+const TeamsSection = () => {
+
+    return (
+        <div className={classes["teams-sec-container"]}>
+            <h1>Teams Section</h1>
+        </div>
+    );
+}
+
+export default TeamsSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LandingCard from '../components/UI/Cards/LandingCard'
+import LandingSection from '../components/LandingSection/LandingSection'
+import TeamsSection from '../components/TeamsSection/TeamSection'
 const Home = () => {
     return (
         <>
         <LandingCard />
-            
+        <LandingSection />
+        <TeamsSection />
         </>
     )
 }
