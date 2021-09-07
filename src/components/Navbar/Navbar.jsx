4659c65365aded11f26b8c378fb6981c5993ef68
shoebilyas123 <shoebilyas123@gmail.com>
2021-09-07 21:41:20 +0530

    Fixed useEffect() error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,58 +1,66 @@
-import React,{useState,useRef,useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import classes from "./Navbar.module.css";
 import NavBrand from "./NavBrand";
 import NavItem from "./NavItem";
 import NavActive from "./NavActive";
 
 const Navbar = () => {
-
   const [pos, setpos] = useState(0);
   const [Click, setClick] = useState(0);
   const Buttonref = useRef(null);
 
-  const setactive = (e,index) => {
+  const setactive = (e, index) => {
     setClick(index);
-  }
+  };
 
   useEffect(() => {
     console.log(Buttonref.current.getBoundingClientRect());
     let right = Buttonref.current.getBoundingClientRect().x;
     setpos({
       right: right,
-      width: Buttonref.current.getBoundingClientRect().width
+      width: Buttonref.current.getBoundingClientRect().width,
     });
-  });
+  }, [Click]);
 
   const NavInfo = [
     {
-      "to" : "/",
-      "name" : "Home",
+      to: "/",
+      name: "Home",
     },
     {
-      "to" : "/teams",
-      "name" : "Teams",
+      to: "/teams",
+      name: "Teams",
     },
     {
-      "to" : "/leads",
-      "name" : "Leads",
+      to: "/leads",
+      name: "Leads",
     },
     {
-      "to" : "/events",
-      "name" : "Events",
-    }
-  ]
+      to: "/events",
+      name: "Events",
+    },
+  ];
 
   return (
     <nav className={classes["nav-container"]}>
       <NavBrand logo={"/assets/ieeecs_logo.svg"} />
       <div className={classes["nav-links"]}>
-        <NavActive right={pos.right} width={pos.width} styles={{right: {pos}}}/>
+        <NavActive
+          right={pos.right}
+          width={pos.width}
+          styles={{ right: { pos } }}
+        />
         {NavInfo.map((item, index) => {
-          return(
-            <NavItem key={index} innerRef ={index === Click? Buttonref : null} to={item.to} onClick={(e) => setactive(e,index)}>
+          return (
+            <NavItem
+              key={index}
+              innerRef={index === Click ? Buttonref : null}
+              to={item.to}
+              onClick={(e) => setactive(e, index)}
+            >
               {item.name}
             </NavItem>
-          )
+          );
         })}
       </div>
     </nav>
