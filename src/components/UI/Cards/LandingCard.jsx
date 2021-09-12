acacf26a1c6a7643823f7b5f4ca4e09bb96a2b6b
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-09-12 13:11:41 +0530

    Added a messed up landing page

--- src/components/UI/Cards/LandingCard.jsx
+++ src/components/UI/Cards/LandingCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import classes from "./Cards.module.css";
 const LandingCard = (props) => {
-  return <div className={classes["landing-card"]}>{props.children}</div>;
+  return (
+    <div className={classes["landing-card"]}>
+      <div className={classes["logo-svg-text"]}>
+        <div className={classes["logo-text"]}>
+          <img src="assets/landingLogo.png" alt="Landing Logo" style={{width: "35vw", height: "auto"}}/>
+          <div className={classes.text}>
+            <p>IEEE CS is a technology driven society established to empower people to bring about changes in the community. We are expanding group of professionals that lead in computing and connecting members worldwide. Our society enables people to advance in technology by delivering tools for individuals at all stages of their professional careers.</p>
+          </div>
+        </div>
+        <div className={classes.svg}>
+          <img src="assets/HeroGraphic.svg" alt="svg" style={{width: "30vw", height: "auto"}}/>
+        </div>
+      </div>
+      <div className={classes.letsGo}>
+        <p>Let's go&gt;&gt;</p>
+      </div>
+    </div>
+  );
 };
 
 export default LandingCard;
