e6ada75a5a8c0da0899f628f2be9d5b2553f86d6
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-09-14 20:52:51 +0530

    Hero

--- src/components/UI/Cards/LandingCard.jsx
+++ src/components/UI/Cards/LandingCard.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import classes from "./Cards.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as icons from "@fortawesome/free-solid-svg-icons";
+
+import { motion } from "framer-motion";
+
 const LandingCard = (props) => {
+  const graphicdiv = useRef(null);
+  const [graphic, setgraphic] = useState(0);
+
+  const graphicpos = (x) => {
+    const rect = graphicdiv.current.getBoundingClientRect();
+    console.log(rect);
+    setgraphic((x - rect.left - rect.width / 2) / 20);
+  };
+
   return (
     <div className={classes["landing-card"]}>
       <div className={classes["logo-svg-text"]}>
         <div className={classes["logo-text"]}>
-          <img src="assets/landingLogo.png" alt="Landing Logo" style={{width: "35vw", height: "auto"}}/>
+          <img src="assets/landingLogo.png" alt="Landing Logo" />
           <div className={classes.text}>
-            <p>IEEE CS is a technology driven society established to empower people to bring about changes in the community. We are expanding group of professionals that lead in computing and connecting members worldwide. Our society enables people to advance in technology by delivering tools for individuals at all stages of their professional careers.</p>
+            <p>
+              IEEE CS is a technology driven society established to empower
+              people to bring about changes in the community. We are expanding
+              group of professionals that lead in computing and connecting
+              members worldwide. Our society enables people to advance in
+              technology by delivering tools for individuals at all stages of
+              their professional careers.
+            </p>
+          </div>
+          <div className={classes["link"]}>
+            <span>
+              <a href="#">Let's Go</a>
+              <span id={classes["icon"]}>
+              <FontAwesomeIcon icon={icons.faAngleDoubleRight} />
+              </span>
+            </span>
           </div>
         </div>
-        <div className={classes.svg}>
-          <img src="assets/HeroGraphic.svg" alt="svg" style={{width: "30vw", height: "auto"}}/>
+        <div className={classes["svg"]}>
+          <div className={classes["image"]}>
+            <motion.div
+              ref={graphicdiv}
+              className="graphic"
+              onMouseLeave={() => setgraphic(0)}
+              onMouseMove={(e) => {
+                graphicpos(e.clientX);
+              }}
+              animate={{
+                rotateX: 0,
+
+                rotateZ: graphic,
+                originY: 0.5,
+                originX: 0.57,
+              }}
+              transition={{ type: "spring", stiffness: 200 }}
+            >
+              <img src="assets/HeroGraphic.png" />
+            </motion.div>
+            <img className={classes["ground"]} src="assets/HeroGround.svg" />
+          </div>
         </div>
-      </div>
-      <div className={classes.letsGo}>
-        <p>Let's go&gt;&gt;</p>
       </div>
     </div>
   );
