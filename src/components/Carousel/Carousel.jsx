1d4f4700e1416eb92e1f018512f4a01e04881365
Husain Shahid Rao <husain@pop-os.localdomain>
2021-09-22 18:53:35 +0530

    add carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import classes from './Carousel.module.css'
-
+import React from "react";
+import classes from "./Carousel.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel as CarouselRrc, Thumbs } from "react-responsive-carousel";
 const Carousel = (props) => {
+  const options = {
+    autoPlay: false,
+    emulateTouch: true,
+    infiniteLoop: true,
+    showStatus: false,
+    renderArrowPrev: (onClickHandler, hasPrev, label) => (
+      <button className={`${classes["arrow"]} ${classes["arrow-prev"]}`} onClick={onClickHandler}>
+      <i className="fas fa-chevron-left"></i>
+      </button>
+    ),
+    renderArrowNext: (onClickHandler, hasNext, label) => (
+      <button className={`${classes["arrow"]} ${classes["arrow-next"]}`} onClick={onClickHandler}>
+       <i className="fas fa-chevron-right"></i>
+      </button>
+    ),
+    renderIndicator: (onClickHandler, isSelected, label) => (<div className={`${classes["indicator"]} ${isSelected ? classes["indicator-selected"] : ""}`} onClick={onClickHandler}></div>),
+  };
 
-    return (
-        <section className={classes["carousel-sec-container"]} id="leads">
-            <h1>CAROUSEL SECTION</h1>
-            <p>Background color is just for testing the height of teams section. Feel free to change it when you want.</p>
-        </section>
-    );
-}
+  return (
+    <>
+      <CarouselRrc {...options} className={classes.carousel}>
+        {props.CarouselData.map((item, index) => (
+          <div key={index} className={classes["item-container"]}>
+            <div className={classes["image-section"]}>
+              <img alt={item.name} src={item.image} />
+              <span>{item.name}</span>
+            </div>
+            <div className={classes["carousel-text"]}>
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
+      </CarouselRrc>
+    </>
+  );
+};
 
 export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import LandingCard from '../components/UI/Cards/LandingCard'
-import TeamsSection from '../components/TeamsSection/TeamSection'
-import Carousel from '../components/Carousel/Carousel'
-
+import React from "react";
+import LandingCard from "../components/UI/Cards/LandingCard";
+import TeamsSection from "../components/TeamsSection/TeamSection";
+import Carousel from "../components/Carousel/Carousel";
+const CarouselData = [
+  {
+    image: "/assets/teamMembers/SARFARAZ.jpg",
+    title: "Vice President",
+    name: "Sarfaraz",
+    text: "It gives me immense pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete  University.",
+  },
+  {
+    image: "/assets/teamMembers/SARA.jpg",
+    title: "President",
+    name: "Sara",
+    text: "It gives me immenseimm enseimmenseimm enseimmense pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
+  }, 
+  {
+    image: "/assets/teamMembers/SARFARAZ.jpg",
+    title: "Prime Minister",
+    name: "Sarfaraz",
+    text: "It gives pleasure  in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
+  },
+  {
+    image: "/assets/teamMembers/SARA.jpg",
+    title: "Defence Minister",
+    name: "Sara",
+    text: "It gives me immense pleasure to b pleasure to b pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
+  },
+];
 const Home = () => {
-    return (
-        <>
-        <LandingCard />
-        <TeamsSection  />
-        <Carousel />
-        </>
-    )
-}
+  return (
+    <>
+      <LandingCard />
+      <TeamsSection />
+      <div className="carousel-section">
+        <Carousel CarouselData={CarouselData} />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
