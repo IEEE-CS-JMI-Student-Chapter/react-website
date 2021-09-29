68f243b3494266f35c93116af4c449c94b49d3e0
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2021-09-29 21:53:46 +0530

    optimize for small screens, add hover effect and links for team members

--- src/components/TeamsSection/TabContent.jsx
+++ src/components/TeamsSection/TabContent.jsx
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as icons from "@fortawesome/free-solid-svg-icons";
 import classes from "./TeamsSection.module.css";
 
-const TabContent = (props) => {
-  const [content, showContent] = useState(false);
-  const expandDivHandler = () => {
-    setTimeout(() => {showContent(true);}, 500);
-    
+const TeamMember = (props) => {
+  const member = props.member;
+  const [isLinkActive, setIsLinkActive] = useState(false);
+  const handleMouseEnter = () => {
+    setIsLinkActive(true);
   };
+
+  const handleMouseLeave = () => {
+    setIsLinkActive(false);
+  };
+
   return (
-    <div onLoad={expandDivHandler} className={`${classes["content-container"]} ${content && classes["content-active"]}`}>
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`${classes.member}  ${member.head && classes["team-head"]}`}>
+      <img className={`${classes["member-image"]} ${isLinkActive && classes["hovered"]}`} src={member.img} alt={member.name} />
+      <p className={classes["member-name"]}>{member.name}</p>
+      <div className={`${classes["member-links"]} ${isLinkActive && classes["hovered"]}`}>
+        {member.linkedin && (
+          <a target="_blank" rel="noreferrer" href={member.linkedin}>
+            <FontAwesomeIcon icon={icons.faUserEdit} />
+          </a>
+        )}
+        {member.github && (
+          <a target="_blank" rel="noreferrer" href={member.github}>
+            <FontAwesomeIcon icon={icons.faUserAstronaut} />
+          </a>
+        )}
+        {member.website && (
+          <a target="_blank" rel="noreferrer" href={member.website}>
+            <FontAwesomeIcon icon={icons.faUserCheck} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const TabContent = (props) => {
+  return (
+    <div className={`${classes["content-container"]} ${classes["content-active"]}`}>
       <div className={classes.text}>{props.html}</div>
       <div className={classes["members-container"]}>
-   
         {props.members.map((member) => {
-          return (
-            
-            <div key={member.name} className={`${classes.member}  ${member.head && classes["team-head"]}`}>
-              <img className={classes["member-image"]} src={member.img} alt={member.name} />
-              <p className={classes["member-name"]}>{member.name}</p>
-            </div>
-            
-          );
+          return <TeamMember member={member} key={member.name} />;
         })}
-       
       </div>
     </div>
   );
