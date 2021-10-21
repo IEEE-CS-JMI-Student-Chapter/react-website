0f02bc783e06e5c7ae0a833b1e5f17965b0ab1eb
MOHAMMAD SARFRAZ ALAM <sarf.alam0206@gmail.com>
2021-10-21 18:52:06 +0000

    Added loading contact us

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -1,10 +1,11 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import classes from "./ContactForm.module.css";
 import Button from "../UI/Button/Button";
 import styled from "styled-components";
 import Card from "../UI/Card/Card";
 import axios from 'axios'
-import {withSnackbar ,useSnackbar} from 'react-simple-snackbar'
+import loadingimg from "../../images/Loading.gif"
+import { withSnackbar, useSnackbar } from 'react-simple-snackbar'
 
 
 const options = {
@@ -27,31 +28,29 @@ function ContactForm(props) {
     },
   ];
 
-  const [Subject,setSubject] = useState("");
-  const [Email,setEmail] = useState("");
-  const [Message,setMessage] = useState("");
-  
+  const [Subject, setSubject] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Message, setMessage] = useState("");
+
+  const [loading, setloading] = useState(false);
 
   const [openSnackbar, closeSnackbar] = useSnackbar(options)
 
-  async function sendmessage(e){
+  async function sendmessage(e) {
     e.preventDefault();
-    const res = await axios.post('https://vast-cove-87257.herokuapp.com/',{
-      subject : Subject,
-      mail : Email,
-      message : Message
+    setloading(true);
+    const res = await axios.post('https://vast-cove-87257.herokuapp.com/', {
+      subject: Subject,
+      mail: Email,
+      message: Message
     });
-
-
     openSnackbar('Message Sent Successfully!')
-    
+    setloading(false);
   }
 
 
-  function setDisable()
-  {
-    if(Subject == "" || Email == "" || Message == "")
-    {
+  function setDisable() {
+    if (Subject == "" || Email == "" || Message == "") {
       return true;
     }
 
@@ -60,49 +59,59 @@ function ContactForm(props) {
 
   return (
     <Card>
-      <div className={classes["contactus"]}>
-        <button className={classes.close} onClick={props.onClose}>
-          X
-        </button>
-        <ul>
-          <li>Contact Us</li>
-          <li>
-            {contactUs.map((contact, index) => {
-              return (
-                <div key={index} className={classes["contactCard"]}>
-                  <div>
-                    {contact.name}
-                  </div>
-                  <div>
-                    {contact.value}
-                  </div>
-                </div>
-              );
-            })}
-          </li>
-        </ul>
-        <form className={classes.sendmessage} onSubmit={(event) => sendmessage(event)}>
-          <label htmlFor="email-id">Email: </label>
-          <input id="email-id" placeholder="Enter e-mail..." value={Email} onChange={(e) => setEmail(e.target.value)}/>
+        {loading ?
 
-          <label htmlFor="subject-id">Subject: </label>
-          <input id="subject-id" placeholder="Enter subject..." value={Subject} onChange={(e)=> setSubject(e.target.value)}/>
-          
-          <label htmlFor="subject-id">Message: </label>
-          <textarea
-            name="message"
-            id="message-id"
-            cols="30"
-            rows="10"
-            placeholder="Message"
-            value={Message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-          <Button disabled={setDisable()}>Send Message</Button>
-        </form>
-      </div>
+          <div className={classes["contactus"]}>
+
+          <div className={classes["loading"]}>
+            <img src={loadingimg} />
+          </div>
+          </div>
+          :
+          <div className={classes["contactus"]}>
+            <button className={classes.close} onClick={props.onClose}>
+              X
+            </button>
+            <ul>
+              <li>Contact Us</li>
+              <li>
+                {contactUs.map((contact, index) => {
+                  return (
+                    <div key={index} className={classes["contactCard"]}>
+                      <div>
+                        {contact.name}
+                      </div>
+                      <div>
+                        {contact.value}
+                      </div>
+                    </div>
+                  );
+                })}
+              </li>
+            </ul>
+            <form className={classes.sendmessage} onSubmit={(event) => sendmessage(event)}>
+              <label htmlFor="email-id">Email: </label>
+              <input id="email-id" placeholder="Enter e-mail..." value={Email} onChange={(e) => setEmail(e.target.value)} />
+
+              <label htmlFor="subject-id">Subject: </label>
+              <input id="subject-id" placeholder="Enter subject..." value={Subject} onChange={(e) => setSubject(e.target.value)} />
+
+              <label htmlFor="subject-id">Message: </label>
+              <textarea
+                name="message"
+                id="message-id"
+                cols="30"
+                rows="10"
+                placeholder="Message"
+                value={Message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              <Button disabled={setDisable()}>Send Message</Button>
+            </form>
+          </div>
+        }
     </Card>
   );
 }
 
-export default withSnackbar(ContactForm,options);
+export default withSnackbar(ContactForm, options);
