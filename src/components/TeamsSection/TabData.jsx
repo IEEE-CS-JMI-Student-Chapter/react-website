073636ceba0c78079f3a195f01e1f5176bc24ece
Husain Shahid Rao <husain3012@pop-os.localdomain>
2021-10-25 21:01:53 +0530

    update content

--- src/components/TeamsSection/TabData.jsx
+++ src/components/TeamsSection/TabData.jsx
@@ -5,12 +5,8 @@ const tabs = [
     html: (
       <div>
         <p>
-          Data Structure and Algorithm is a prime branch of Computer Science that deals with creating machine-efficient and optimised computer programs. Nearly, each and every tech giant, be it Amazon, Google, Microsoft or any other top
-          tech company, recruits professionals on the basis of their mastery in this field. Having said that, it is highly recommended to have a profound knowledge of the same.
-        </p>
-        <p>
-          On similar grounds, The Data Structure and Algorithm (DSA) Section of IEEE CS JMI aims to assist individuals in focusing on number theory, computational analysis, Google coding interviews and each and every other area of this
-          field, which would help them become a strong and skilled coder, and acquire efficiency in optimising space and time as well.
+          Data Structure and Algorithm is a prime branch of Computer Science. It deals with creating machine-efficient and optimized computer programs. Nearly, each and every tech giant, be it Amazon, Google, Microsoft or any other top tech
+          company, recruits professionals on the basis of their mastery in this field.
         </p>
       </div>
     ),
@@ -19,23 +15,23 @@ const tabs = [
       {
         name: "Anzal Husain Abedi",
         head: true,
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/anzal.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "",
         website: "",
       },
       {
-        name: "Fardeen Something",
+        name: "Fardeen Husain",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/fardeen.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
       },
       {
-        name: "Fazeel Something",
+        name: "Fazeel Rizvi",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/fazeel.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -43,7 +39,7 @@ const tabs = [
       {
         name: "Lal Bihari Pandey",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/lal-bihari.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -51,7 +47,7 @@ const tabs = [
       {
         name: "Md. Arbaaz",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/arbaaz.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -63,13 +59,10 @@ const tabs = [
     title: "ML/AI",
     html: (
       <div>
-        <p>"AI is the new electricity. It will transform every industry and create huge economic value."</p>
-        <p>- Andrew Ng (Founder & CEO of LandingAI).</p>
         <p>
-          The AI/ML team at IEEE CS aspires to build a strong community of support for ML enthusiasts, impart accurate knowledge and awareness, and give a hands-on introduction to this field, taking them from having no idea about ML to
-          competing in competitions. These competitions will range from generating the best ML model on datasets to creating end-to-end deployable ML products.{" "}
+          The AI/ML team at IEEE CS aims to build a strong community of support for ML enthusiasts. JMI students will be able to compete in competitions, from generating the best ML model on datasets to creating end-to-end deployable ML
+          products. "AI is the new electricity," says Andrew Ng (Founder & CEO of LandingAI).
         </p>
-        <p>The AI/Ml team of IEE CS strives to nurture the students at JMI to become trailblazers in this field, guide them through their troubles, and help reap economic opportunities.</p>
       </div>
     ),
 
@@ -77,7 +70,7 @@ const tabs = [
       {
         name: "Mohammed Abbas Ansari",
         head: true,
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/abbas.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -85,7 +78,7 @@ const tabs = [
       {
         name: "Ayaz Zia Ansari",
 
-        img: "assets/teamMembers/AYAZ.jpg",
+        img: "assets/teamMembers/ayaz.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -93,7 +86,7 @@ const tabs = [
       {
         name: "Md. Haider Zama",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/haider.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -101,7 +94,7 @@ const tabs = [
       {
         name: "Mohammed Kashif",
 
-        img: "assets/teamMembers/ABBAS.jpg",
+        img: "assets/teamMembers/kashif.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -113,12 +106,10 @@ const tabs = [
     title: "WebD",
     html: (
       <div>
-        <p>In the race of getting IT jobs, most of the students solely focus on DS and forget about a crucial part that is projects.</p>
         <p>
-          We, IEEECS, aim to provide exposure to the aspirants about the world of development and designing where they can get the best knowledge & experience before they really start working. So it is very important to have a grasp over
-          things beforehand.
+          We aim to provide exposure to the aspirants about the world of development and design. Our Web Development team aim to give aspirants the right kind of exposure, learning environment and on point industry requirements to get
+          started. We, IEEECS, aim to provide exposure to the aspirants about the world of development and designing where they can get the best knowledge & experience before they really start working.
         </p>
-        <p>That is why we, as a Web Development team aim to give aspirants the right kind of exposure, learning environment with on point industry requirements to get started and show the world their real calibre.</p>
       </div>
     ),
 
@@ -126,7 +117,7 @@ const tabs = [
       {
         name: "Husain Shahid Rao",
         head: true,
-        img: "assets/teamMembers/TAHIR.jpg",
+        img: "assets/teamMembers/husain.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -134,7 +125,7 @@ const tabs = [
       {
         name: "Tahir Reyaz",
 
-        img: "assets/teamMembers/TAHIR.jpg",
+        img: "assets/teamMembers/tahir.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -142,7 +133,7 @@ const tabs = [
       {
         name: "Saquib Ali",
 
-        img: "assets/teamMembers/Saquib.jpeg",
+        img: "assets/teamMembers/saquib.jpeg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -150,7 +141,7 @@ const tabs = [
       {
         name: "Farhan Akhtar",
 
-        img: "assets/teamMembers/FARHAN.jpg",
+        img: "assets/teamMembers/farhan.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
@@ -158,7 +149,7 @@ const tabs = [
       {
         name: "Shoeb Ilyas",
 
-        img: "assets/teamMembers/SHOEB.jpg",
+        img: "assets/teamMembers/shoeb.jpg",
         linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
         github: "www.google.com",
         website: "www.google.com",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,35 +7,35 @@ import Footer from "../components/Footer/footer";
 
 const CarouselData = [
   {
-    image: "/assets/teamMembers/SARFARAZ.jpg",
-    title: "Vice President",
-    name: "Sarfaraz",
-    text: "It gives me immense pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete  University.",
+    image: "/assets/teamMembers/sara.jpg",
+    title: "Chairperson",
+    name: "Sara Batool Hussain",
+    text: 'IEEE CS has enabled me to connect with like-minded and talented individuals and collaborate to create a good platform for tech enthusiasts. We plan to take this society to new heights with our dedication and hard work. It is rightly said by Abigail Adams, "Learning is not attained by chance, it must be sought for with ardour and attended to with diligence."',
   },
   {
-    image: "/assets/teamMembers/SARA.jpg",
-    title: "President",
-    name: "Sara",
-    text: "It gives me immenseimm enseimmenseimm enseimmense pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
-  }, 
-  {
-    image: "/assets/teamMembers/SARFARAZ.jpg",
-    title: "Prime Minister",
-    name: "Sarfaraz",
-    text: "It gives pleasure  in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
+    image: "/assets/teamMembers/sarfraz.jpg",
+    title: "Vice Chairperson",
+    name: "Mohammad Sarfraz Alam",
+    text: "IEEE Computer Society in Jamia Millia Islamia will surely provide a platform where we all learn together and grow as a team. It will provide a platform for many enthusiasts who want to explore various domains such as Machine Learning, Artificial Intelligence, App Development and more. I am optimistic about this society's future.",
   },
   {
-    image: "/assets/teamMembers/SARA.jpg",
-    title: "Defence Minister",
-    name: "Sara",
-    text: "It gives me immense pleasure to b pleasure to b pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete University.",
+    image: "/assets/teamMembers/faizan.jpg",
+    title: "Secretary",
+    name: "Faizan Choudhary",
+    text: "It gives me immense pleasure to be a part of IEEE CS in its reviving years, providing me with the opportunity to lay a strong foundation on the path to achieve and create a concrete platform for young Tech enthusiasts. I wish to contribute to the best of my abilities to help this Society achieve its pinnacle and will aid in accomplishing its goals for the collective benefit of our University.",
+  },
+  {
+    image: "/assets/teamMembers/ayaz.jpg",
+    title: "Technical Supervisor",
+    name: "Ayaz Zia Ansari",
+    text: 'Our college Jamia Millia Islamia lacked an official Computer Science society dedicated entirely to tech enthusiasts. Being freshers, we took the initiative to revive the CS branch of IEEE to provide tech wizards with a community to support each other. We aim to instil young minds with a knack for being indulged in programming. Dave Waters - "Machine learning will automate jobs that most people thought could only be done by people."',
   },
 ];
 const Home = () => {
   return (
     <>
       <LandingCard />
-       <IntroPage />
+      <IntroPage />
       <TeamsSection />
       <div className="carousel-section" id="leads">
         <Carousel CarouselData={CarouselData} />
