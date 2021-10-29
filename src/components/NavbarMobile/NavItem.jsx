599218609be6e74935ee20a6e9efe1c1de23a358
Husain Shahid Rao <husain3012@pop-os.localdomain>
2021-10-29 23:54:15 +0530

    add team members data, resolved active link bug in mobile navbar

--- src/components/NavbarMobile/NavItem.jsx
+++ src/components/NavbarMobile/NavItem.jsx
@@ -3,7 +3,7 @@ import classes from "./NavbarMobile.module.css";
 import { NavHashLink } from "react-router-hash-link";
 const NavItem = (props) => {
   return (
-    <NavHashLink smooth={true} exact={true} activeClassName={classes.active} to={props.to} className={`${classes["nav-item"]}`}>
+    <NavHashLink smooth={true} exact={true} activeClassName={classes.active} to={props.to} className={` ${classes["nav-item"]}`}>
       <span className={classes["nav-icon"]}> {props.icon}</span>
 
       <span className={classes["nav-title"]}>{props.children}</span>
--- src/components/TeamsSection/TabData.jsx
+++ src/components/TeamsSection/TabData.jsx
@@ -16,41 +16,44 @@ const tabs = [
         name: "Anzal Husain Abedi",
         head: true,
         img: "assets/teamMembers/anzal.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "",
-        website: "",
+        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi/",
+        github: "https://github.com/anzal1",
+      },
+
+      {
+        name: "Fazeel Rizvi",
+
+        img: "assets/teamMembers/fazeel.jpg",
+        linkedin: "https://www.linkedin.com/in/fazeel-rizvi-a497651b4/",
+        github: "https://github.com/fazcodev",
+      },
+
+      {
+        name: "Shairin Meraj",
+
+        img: "assets/teamMembers/shairin.jpg",
+        linkedin: "https://www.linkedin.com/in/shairin-meraj-451524202",
+        github: "https://github.com/ShairinMeraj",
+      },
+      {
+        name: "Sparsh Mahajan",
+
+        img: "assets/teamMembers/sparsh.jpg",
+        linkedin: "https://www.linkedin.com/in/sparsh-mahajan-7b979b202",
+        github: "https://github.com/Sparshmahajan",
       },
       {
         name: "Fardeen Husain",
 
         img: "assets/teamMembers/fardeen.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
-      },
-      {
-        name: "Fazeel Rizvi",
-
-        img: "assets/teamMembers/fazeel.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/fardeen-hussain-0479b8202",
       },
       {
         name: "Lal Bihari Pandey",
 
         img: "assets/teamMembers/lal-bihari.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
-      },
-      {
-        name: "Md. Arbaaz",
-
-        img: "assets/teamMembers/arbaaz.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/xpandeyed",
+        github: "https://github.com/xpandeyed",
       },
     ],
   },
@@ -71,33 +74,36 @@ const tabs = [
         name: "Mohammed Abbas Ansari",
         head: true,
         img: "assets/teamMembers/abbas.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
-      },
-      {
-        name: "Ayaz Zia Ansari",
-
-        img: "assets/teamMembers/ayaz.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/abbas-ansari-2001/",
+        github: "https://github.com/King-of-Haskul",
       },
       {
         name: "Md. Haider Zama",
 
         img: "assets/teamMembers/haider.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/md-haider-zama-3937a8218/",
+        github: "https://github.com/haiderzm",
       },
       {
         name: "Mohammed Kashif",
 
         img: "assets/teamMembers/kashif.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/mohammad-kashif-2b7429210/",
+        github: "https://github.com/M0hammad-Kashif",
+      },
+      {
+        name: "Tirth Dalwadi",
+
+        img: "assets/teamMembers/tirth.jpg",
+        linkedin: "https://www.linkedin.com/in/tirth-dalwadi-0513911b7",
+        github: "https://github.com/Tirth-AI",
+      },
+      {
+        name: "Abbas Haider",
+
+        img: "assets/teamMembers/abbas-haider.jpg",
+        linkedin: "https://www.linkedin.com/in/abbas-haider-96042b217",
+        github: "https://github.com/abbash77",
       },
     ],
   },
@@ -118,41 +124,35 @@ const tabs = [
         name: "Husain Shahid Rao",
         head: true,
         img: "assets/teamMembers/husain.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/husain3012/",
+        github: "https://github.com/husain3012",
       },
       {
         name: "Tahir Reyaz",
 
         img: "assets/teamMembers/tahir.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/tahir-reyaz-a9aa2a202/",
+        github: "https://github.com/TahirReyaz",
       },
       {
         name: "Saquib Ali",
 
-        img: "assets/teamMembers/saquib.jpeg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        img: "assets/teamMembers/saquib.jpg",
+        linkedin: "https://www.linkedin.com/in/saquib-ali-89aa14202",
+        github: "http://github.com/Saquibali7",
       },
       {
         name: "Farhan Akhtar",
 
         img: "assets/teamMembers/farhan.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/mohd-farhan-akhtar-193194202/",
+        github: "https://github.com/farhanakhtar24",
       },
       {
         name: "Shoeb Ilyas",
-
         img: "assets/teamMembers/shoeb.jpg",
-        linkedin: "https://www.linkedin.com/in/anzal-husain-abedi-b9a8b9b4/",
-        github: "www.google.com",
-        website: "www.google.com",
+        linkedin: "https://www.linkedin.com/in/shoeb-ilyas/",
+        github: "https://github.com/shoebilyas123",
       },
     ],
   },
