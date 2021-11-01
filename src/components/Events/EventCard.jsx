42b0618dc31569e09e8c54f63263729a25f44087
Husain Shahid Rao <husain3012@pop-os.localdomain>
2021-11-01 13:17:13 +0530

    event card change

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,37 +1,45 @@
-import React,{useState} from 'react'
-import classes from './Events.module.css'
-import { useHistory } from 'react-router';
+import React, { useState } from "react";
+import classes from "./Events.module.css";
+import { useHistory } from "react-router";
+import styled from "styled-components";
+
+const EventBackground = styled.div`
+  background-image: ${(props) => props.bg && `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${(props) => props.bg})`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
 
 const EventCard = (props) => {
-    const history = useHistory();
-    const [btnhover,setbtn] = useState(false);
-    
+  const history = useHistory();
+  const [btnhover, setbtn] = useState(false);
 
-    return (
-        <div className={classes["event-card"]}>
-            <div className={classes["event-details"]}>
-                <p style={{marginBlockStart: "0"}}>{props.EventName}</p>
-                <p style={{marginBlockStart: "0"}}>Date: <span>{props.Date}</span>
-                </p>
-            </div>
-            <div className={classes["button-and-winner"]}>
-                <span>{props.winner}</span>
-                <button className={classes[`event-btn`]} 
-                style={{
-                    backgroundColor: btnhover? "white" : props.btnColor,
-                    color: btnhover? props.btnColor: "white"
-                }}
-                onMouseEnter={() => setbtn(true)} 
-                onMouseLeave={() => setbtn(false)} 
-                onClick = {() => {
-                    history.push(`events/${props.id}`)
-                }}
-                >
-                    {props.btnText}
-                </button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <EventBackground bg={"https://images.unsplash.com/photo-1635701248071-ed01d3622db3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=762&q=80"} className={classes["event-card"]}>
+      <div className={classes["event-details"]}>
+        <h3>{props.EventName}</h3>
+        <p>
+          Date: <span>{props.Date}</span>
+        </p>
+      </div>
+      <div className={classes["button-and-winner"]}>
+        <button
+          className={classes[`event-btn`]}
+          style={{
+            backgroundColor: btnhover ? "white" : props.btnColor,
+            color: btnhover ? props.btnColor : "white",
+          }}
+          onMouseEnter={() => setbtn(true)}
+          onMouseLeave={() => setbtn(false)}
+          onClick={() => {
+            history.push(`events/${props.id}`);
+          }}
+        >
+          {props.btnText}
+        </button>
+      </div>
+    </EventBackground>
+  );
+};
 
 export default EventCard;
--- src/components/Events/SingleEvent.jsx
+++ src/components/Events/SingleEvent.jsx
@@ -1,51 +1,39 @@
-import React,{useEffect,useState} from "react"
-import { useParams } from "react-router"
-import style from "./singleevent.module.css"
-import loading  from "../../images/Loading.gif"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import style from "./singleevent.module.css";
+import loading from "../../images/Loading.gif";
+import axios from "axios";
 const SingleEvent = (props) => {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  const [data, setdata] = useState(null);
 
-    const [data,setdata] = useState(null)
-
-    useEffect(async () => {
-        if(id !== undefined)
-        {
-
-            const info = await axios.get(`https://ieeecsbackend.herokuapp.com/events/${id}`)
-            console.log(info.data)
-            setdata(info.data)
-        
-        }
-    },[])
-
-    const setloading = () => {
-        return (
-            <div className={style['loading']}>
-                <img src={loading} alt="loading" />
-            </div>
-        )
+  useEffect(async () => {
+    if (id !== undefined) {
+      const info = await axios.get(`https://ieeecsbackend.herokuapp.com/events/${id}`);
+      console.log(info.data);
+      setdata(info.data);
     }
+  }, []);
 
-    const EventData = () => {
-        return(
-            <div className={style['card']}>
-                <h1>{data.EventName}</h1>
-                <p>
-                    {data.Info}
-                </p>
-            </div>
-        )
-    }
-
+  const setloading = () => {
     return (
-        <div className={style['container']}>
-            {data == null ? setloading() : EventData()}
+      <div className={style["loading"]}>
+        <img src={loading} alt="loading" />
+      </div>
+    );
+  };
 
-        </div>
-    )
+  const EventData = () => {
+    return (
+      <div className={style["card"]}>
+        <h1>{data.EventName}</h1>
+        <p>{data.Info}</p>
+      </div>
+    );
+  };
 
-}
+  return <div className={style["container"]}>{data == null ? setloading() : EventData()}</div>;
+};
 
-export default SingleEvent
+export default SingleEvent;
